2069405305854edee0ff45bbbc2d23bcabb44a35
Lexi Larkin <lexi@qixils.dev>
2026-03-13 09:40:44 -0400

    feat: padding & colors

--- client/src/pages/admin/+Page.vue
+++ client/src/pages/admin/+Page.vue
@@ -46,31 +46,31 @@ const replaceMirror = async () => {
 <template>
   <div class="flex flex-col gap-1">
     <p>Secret admin dashboard for updating data.</p>
-    <div class="grid grid-cols-[auto_1fr] gap-1 items-center">
+    <div class="grid grid-cols-[auto_1fr] gap-1 items-center bg-emerald-800 p-1">
       <p>Password</p>
-      <input type="password" v-model="adminSecret" />
+      <input type="password" v-model="adminSecret" class="bg-emerald-900 px-2 py-1.5 rounded" />
     </div>
 
     <p>Last tRPC response</p>
-    <p class="bg-emerald-400 rounded p-1 border-emerald-600">{{ lastTrpcResult }}</p>
+    <p class="bg-emerald-800 rounded p-1 border border-emerald-600">{{ lastTrpcResult }}</p>
 
     <p>Add/replace video mirror</p>
-    <div class="grid grid-cols-[auto_1fr] gap-1 items-center">
+    <div class="grid grid-cols-[auto_1fr] gap-1 items-center bg-emerald-800 p-1">
       <p>Video ID</p>
-      <input type="text" v-model.number="videoId" />
+      <input type="text" v-model.number="videoId" class="bg-emerald-900 px-2 py-1.5 rounded" />
 
       <p>Mirror Source</p>
-      <select v-model="mirrorType">
+      <select v-model="mirrorType" class="bg-emerald-900 px-2 py-1.5 rounded">
         <option value="INTERNET_ARCHIVE">Internet Archive</option>
         <option value="YOUTUBE">YouTube</option>
       </select>
 
       <p>Video URL</p>
-      <input type="text" v-model.trim="videoUrl" />
+      <input type="text" v-model.trim="videoUrl" class="bg-emerald-900 px-2 py-1.5 rounded" />
 
       <div class="col-span-2 flex flex-row gap-1">
-        <button @click="addMirror">Add</button>
-        <button @click="replaceMirror">Replace</button>
+        <button @click="addMirror" class="bg-emerald-800 px-2 py-1.5 rounded">Add</button>
+        <button @click="replaceMirror" class="bg-emerald-800 px-2 py-1.5 rounded">Replace</button>
       </div>
     </div>
   </div>
